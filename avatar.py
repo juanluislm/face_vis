e6d3c3c5a38e6d30be5ce8c3cd5687a47ebe1f4f
azhongwl <azhong@AF051506.employee-wifi.rwc.corp.imvu.com>
2018-08-24 10:46:47 -0700

    export custom matrix through custom joint moving function; however not working on arface yet; the custom shapes seem to be applied hierachically

--- avatar.py
+++ avatar.py
@@ -1,7 +1,7 @@
 import os
 import json
 import numpy as np
-from math_lib import translate, scale, rotate
+from math_lib import translate, scale, rotate, transform_to_translate_rotation_scale, translateM_to_vector, scalingM_to_vector, rotateM_to_vector
 
 class AvatarController:
     def __init__(self, json_path):
@@ -28,7 +28,7 @@ class AvatarController:
         assert controller_name in self.controller_to_scale_pivot, '{} not in controller'.format(controller_name)
         return self.controller_to_scale_pivot[controller_name]
 
-    def get_joint(self, controller_name):
+    def get_joint_short_names(self, controller_name):
         assert controller_name in self.controller_to_joints, '{} not in controller'.format(controller_name)
         return self.controller_to_joints[controller_name]
 
@@ -39,11 +39,16 @@ class AvatarController:
         joints = sorted(list(set(joints)))
         return joints
 
+    def get_controllers(self):
+        return sorted(self.controller_to_joints.keys())
+
 class AvatarAnimationHierarchy:
     def __init__(self, json_path):
         data = json.load(open(json_path))['hierarchy']
         self.joint_short_to_full = {}
         self.joint_dict = {}
+        self.global_transform_dict = {}
+        self.local_transform_dict = {}
         for i in data:
             name = i['name']
             short_name = name.split('|')[-1]
@@ -51,6 +56,28 @@ class AvatarAnimationHierarchy:
             self.joint_short_to_full[short_name] = name
             assert name not in self.joint_dict
             self.joint_dict[name] = i
+
+    def load_custom_shapes(self, json_path):
+        data = json.load(open(json_path))['custom_shapes']
+        for custom_shape in data:
+            name = custom_shape['name']
+            translationMat = translate(custom_shape['translation'])
+            rotationMat = rotate(custom_shape['rotation'])
+            scaleMat = scale(np.array(custom_shape['scaling']) + np.array([1,1,1]))
+            customMat = np.dot(np.dot(translationMat, rotationMat), scaleMat)
+            self.local_transform_dict[name] = customMat
+
+    def export_custom_shapes(self, json_path):
+        custom_shapes_dict = {"custom_shapes": []}
+        for name in self.local_transform_dict.keys():
+            custom_shape = {}
+            custom_shape["name"] = name
+            t, r, s = transform_to_translate_rotation_scale(self.local_transform_dict[name])
+            custom_shape["rotation"] = list(rotateM_to_vector(r))
+            custom_shape["scaling"] = list(scalingM_to_vector(s))
+            custom_shape["translation"] = list(translateM_to_vector(t))
+            custom_shapes_dict["custom_shapes"].append(custom_shape)
+        json.dump(custom_shapes_dict, open(json_path, 'w'), indent = '    ')
 
     def get_name_by_short_name(self, short_name):
         return self.joint_short_to_full[short_name]
@@ -62,7 +89,9 @@ class AvatarAnimationHierarchy:
     def get_joint_by_name(self, name):
         return self.joint_dict[name]
 
-    def get_transform(self, name):
+    def get_global_transform(self, name):
+        if name in self.global_transform_dict:
+            return self.global_transform_dict[name]
         joint = self.get_joint_by_name(name)
         # use the joint's world info for parent
         if joint['parents'] == []:
@@ -74,7 +103,7 @@ class AvatarAnimationHierarchy:
             parentRotationMatrix = rotate(rotationDiff)
             parentTotalMatrix = np.dot(np.dot(parentTranslationMatrix, parentRotationMatrix), parentScaleMatrix)
         else:
-            parentTotalMatrix = self.get_transform(joint['parents'])
+            parentTotalMatrix = self.get_global_transform(joint['parents'])
 
         translationMat = translate(joint['position'])
         scaleMat = scale(joint['scale'])
@@ -88,4 +117,22 @@ class AvatarAnimationHierarchy:
         mpTotalMatrix = np.dot(parentTotalMatrix, np.dot(mpLocalMatrix, mpOrientationMatrix))
         diff = np.linalg.norm(np.array([mpTotalMatrix[0][3], mpTotalMatrix[1][3], mpTotalMatrix[2][3]])-np.array(joint['world_position']))
         assert diff < 1e-9, 'sanity check fails: computed joint transform does not conform to world position, diff norm: {}'.format(diff)
+        self.global_transform_dict[name] = mpTotalMatrix
         return mpTotalMatrix
+
+    def get_local_transform(self, name):
+        return self.local_transform_dict[name]
+
+    def get_transform(self, name):
+        return np.dot(self.get_global_transform(name), self.get_local_transform(name))
+
+    def move_joint(self, name, dx, dy, dz):
+        transform = self.get_transform(name)
+        global_transform = self.get_global_transform(name)
+        new_transform = transform.copy()
+        new_transform[0][3] += dx
+        new_transform[1][3] += dy
+        new_transform[2][3] += dz
+        # global_transform * new_local_transform = new_transform
+        new_local_transform = np.dot(np.linalg.inv(global_transform), new_transform)
+        self.local_transform_dict[name] = new_local_transform
--- math_lib.py
+++ math_lib.py
@@ -21,3 +21,48 @@ def rotate2(rotate_in_degrees):
     rotateY, _ = cv2.Rodrigues(np.array([0, rotate_in_degrees[1]/180*math.pi, 0]))
     rotateZ, _ = cv2.Rodrigues(np.array([0, 0, rotate_in_degrees[2]/180*math.pi]))
     return np.dot(np.dot(rotateZ, rotateY), rotateX)
+
+def isRotationMatrix(m):
+    mt = np.transpose(m)
+    shouldBeIdentity = np.dot(mt, m)
+    I = np.identity(4, dtype = m.dtype)
+    n = np.linalg.norm(I - shouldBeIdentity)
+    return n < 1e-6
+
+def rotationMatrixToEulerAnglesInDegrees(m) :
+    assert(isRotationMatrix(m))
+    sy = math.sqrt(m[0,0] * m[0,0] +  m[1,0] * m[1,0])
+    singular = sy < 1e-6
+    if  not singular :
+        x = math.atan2(m[2,1] , m[2,2])
+        y = math.atan2(-m[2,0], sy)
+        z = math.atan2(m[1,0], m[0,0])
+    else :
+        x = math.atan2(-m[1,2], m[1,1])
+        y = math.atan2(-m[2,0], sy)
+        z = 0
+    return np.array([x/math.pi*180, y/math.pi*180, z/math.pi*180])
+
+def transform_to_translate_rotation_scale(m):
+    a, b, c, d = m[0]
+    e, f, g, h = m[1]
+    i, j, k, l = m[2]
+    translateM = translate([d, h, l])
+    sx = np.linalg.norm([a, e, i])
+    sy = np.linalg.norm([b, f, j])
+    sz = np.linalg.norm([c, g, k])
+    rotateM = np.array([[a/sx, b/sy, c/sz, 0],
+                        [e/sx, f/sy, g/sz, 0],
+                        [i/sx, j/sy, k/sz, 0],
+                        [0,    0,    0,    1]])
+    scaleM = scale([sx, sy, sz])
+    return translateM, rotateM, scaleM
+
+def translateM_to_vector(m):
+    return np.array([m[0][3], m[1][3], m[2][3]])
+
+def rotateM_to_vector(m):
+    return rotationMatrixToEulerAnglesInDegrees(m)
+
+def scalingM_to_vector(m):
+    return np.array([m[0][0]-1, m[1][1]-1, m[2][2]-1])
